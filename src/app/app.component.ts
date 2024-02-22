4959efce269ddbc0c294242a16cad4ca481cdee7
Roland Tubongye W <roland.beni1@gmail.com>
2024-02-22 16:43:48 -0500

    [UPDATE] add test values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Order, ProductOrder, Store, User} from "./services/invoice.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'write-pdf';
+
+  store: Store = {
+    name: 'Udacity',
+    address: {
+      street: 'Ste. Catherine',
+      city: 'Montreal, QC',
+      country: 'CA'
+    },
+    email: 'test@udacity.com'
+  };
+  order: Order = {
+    address: {
+      street: '6530 Av. De Chateaubriand',
+      city: 'Montreal',
+      country: 'CA'
+    },
+    createdAt: new Date(),
+    orderedAt: new Date()
+  };
+  user: User = {
+    name: 'John Doe',
+    emailAddress: 'johndoe@test.com',
+    phoneNumber: '+1 514 000 0001'
+  }
+  products: ProductOrder[] = [
+    {
+      name: 'PoutineVille - Wall Street',
+      quantity: 1,
+      price: 120,
+      passengerName: 'John Doe',
+      metaDatas: {
+        name: 'Seat',
+        metaDataValues: {
+          value: 7
+        }
+      }
+    }
+  ]
 }
