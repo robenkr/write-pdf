4d1172f9f5db0b6de5df4c40d3ddce4bfbb85617
Roland Tubongye W <roland.beni1@gmail.com>
2024-02-28 23:01:30 -0500

    [UPDATE] call generateTicketInvoice() method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Order, ProductOrder, Store, User} from "./services/invoice.service";
+import {InvoiceService} from "./services/invoice.service";
 
 @Component({
   selector: 'app-root',
@@ -9,41 +10,65 @@ import {Order, ProductOrder, Store, User} from "./services/invoice.service";
 export class AppComponent {
   title = 'write-pdf';
 
+  /**
+   * Test datas
+   * ==========
+  */
   store: Store = {
     name: 'Udacity',
-    address: {
+    storeAddress: [{
       street: 'Ste. Catherine',
       city: 'Montreal, QC',
       country: 'CA'
-    },
+    }],
     email: 'test@udacity.com'
   };
   order: Order = {
+    uid: 'ifhbkjfdhvuevu',
+    orderShortCode: 'REF-JR9BY',
     address: {
       street: '6530 Av. De Chateaubriand',
       city: 'Montreal',
       country: 'CA'
     },
+    comment: 'The product will be there soon !',
     createdAt: new Date(),
     orderedAt: new Date()
   };
   user: User = {
     name: 'John Doe',
     emailAddress: 'johndoe@test.com',
-    phoneNumber: '+1 514 000 0001'
-  }
+    phoneNumber: '+1 514 000 0001',
+    address: [
+      {
+        street: '1200 Atwater Ave, Westmount, Quebec H3Z 1T4',
+        city: 'Montreal, QC',
+        country: 'CA'
+      }
+    ]
+  };
   products: ProductOrder[] = [
     {
       name: 'PoutineVille - Wall Street',
       quantity: 1,
       price: 120,
       passengerName: 'John Doe',
-      metaDatas: {
+      metaDatas: [{
         name: 'Seat',
-        metaDataValues: {
+        metaDataValues: [{
           value: 7
-        }
-      }
+        }]
+      }]
     }
-  ]
+  ];
+ /**
+  * ==========
+  */
+
+  constructor(private invoiceService: InvoiceService) {
+  }
+
+  generatePDF(): void {
+    this.invoiceService.generateTicketInvoice(this.order, this.store, this.user, this.products);
+  }
 }
